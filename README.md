2c876ea04e72923c2b1233cdbce3213165632445
e1000-io <emiliodelatorrea@gmail.com>
2017-10-25 22:32:29 +0100

    Remove unused packages & adding travis ci

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # mlb-stat-api
 GraphQL API for MLB Stats
+
+[![Build Status](https://travis-ci.org/meel-io/mlb-stat-api.svg?branch=master)](https://travis-ci.org/meel-io/mlb-stat-api) ![devDependencies](https://david-dm.org/meel-io/mlb-stat-api/dev-status.svg) ![dependencies](https://david-dm.org/meel-io/mlb-stat-api.svg)
--- resolvers/index.js
+++ resolvers/index.js
@@ -1,25 +1,9 @@
 const { merge } = require('lodash')
-const { createError } = require('apollo-errors')
 
-const BaseResolver = require('./base')
 const SeasonResolver = require('./season')
 const LeagueResolver = require('./league')
 const DivisionResolver = require('./division')
 const TeamResolver = require('./team')
-
-const DataError = createError('DataError', {
-  message: 'Error with the data server'
-})
-
-const updateDivision = BaseResolver.createResolver(async (root, args, context, info) => {
-  try {
-    let team = await context.db.Team.updateDivision(args.teamId, args.divisionId)
-
-    return team[0]
-  } catch (error) {
-    throw DataError
-  }
-})
 
 const QueryResolver = {
   Query: {
@@ -31,7 +15,11 @@ const QueryResolver = {
 
 const MutationResolver = {
   Mutation: {
-    updateDivision
+    async updateDivision (root, args, context, info) {
+      let team = await context.db.Team.updateDivision(args.teamId, args.divisionId)
+
+      return team[0]
+    }
   }
 }
 
